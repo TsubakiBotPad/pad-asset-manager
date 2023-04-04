5f5df1ca36334dc8664d6bd092de3d24623a04ec
AA <chasehult@gmail.com>
2023-04-03 23:59:05 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pad_asset_manager",
-    version="1.0.1",
+    version="1.0.2",
     author="Aradia Megido#2552 and Cody Watts",
     author_email="chasehult@gmail.com",
     license="MIT",
     description="An asset and extra manager for Puzzle and Dragons",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TsubakiBotPad/tsutils",
+    url="https://github.com/TsubakiBotPad/pad_asset_manager",
     packages=setuptools.find_packages(),
     install_requires=[],
     classifiers=[
@@ -20,5 +20,4 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
 )
